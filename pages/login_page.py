6debd4f8ccf28d66728dd979db129e672ffcbc50
leonbork <leonidborkin@gmail.com>
2025-02-09 16:45:11 +0200

    t2

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,27 +1,12 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 
 
-class LoginPage:
-    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[data-testid='login-button']")
-    EMAIL_INPUT = (By.NAME, "email")
-    PASSWORD_INPUT = (By.NAME, "password")
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
-
-    def __init__(self, driver):
-        self.driver = driver
-
+class LoginPage(BasePage):
     def login(self, username, password):
-        self.driver.get("https://www.terminalx.com/")
+        self.driver.find_element("data-test-id", "qa-header-login-button").click()
+        self.driver.find_element("id", "qa-login-email-input").send_keys(username)
+        self.driver.find_element("id", "qa-login-password-input").send_keys(password)
+        self.driver.find_element("data-test-id", "qa-login-submit").click()
 
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.LOGIN_BUTTON)
-        ).click()
-
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.EMAIL_INPUT)
-        ).send_keys(username)
-
-        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
-        self.driver.find_element(*self.SUBMIT_BUTTON).click()
+    def is_logged_in(self):
+        return "Logout" in self.driver.page_source
